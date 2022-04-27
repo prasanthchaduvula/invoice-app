80507692643d685f26f4540e262bc1bb17ce40ae
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-04-27 17:29:13 +0530

    Inital setup

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+
+import "stylesheets/application";
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+// Makes React dev tools etc, work.
+ReactRailsUJS.mountComponents();
+
+ReactRailsUJS.useContext(componentRequireContext);
---
+++ config/application.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module InvoiceApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
+  end
+end
